952d78d97ffd96a6bb636725f63d398162c71dff
Draym <kevin.draym@gmail.com>
2023-03-14 10:43:06 +0800

    tak: add routes to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,13 @@
 import logRoute from "./log.route"
+import collectionRoute from "./collection.route"
+import modelRoute from "./model.route"
+import tokenRoute from "./token.route"
 
 const routers = [
-    logRoute
+    logRoute,
+    collectionRoute,
+    modelRoute,
+    tokenRoute
 ]
 
 export default routers
