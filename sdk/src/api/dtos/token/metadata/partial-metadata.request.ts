4dfc67a24d2caf88f9e0f0983df4596412519042
Draym <kevin.draym@gmail.com>
2023-04-04 16:16:42 +0800

    task: bump sdk and simplify undefined props

--- sdk/src/api/dtos/token/metadata/partial-metadata.request.ts
+++ sdk/src/api/dtos/token/metadata/partial-metadata.request.ts
@@ -1,8 +1,8 @@
 export default interface PartialMetadataRequest {
-    name: string | undefined
-    description: string | undefined
-    imageUrl: string | undefined
-    externalUrl: string | undefined
-    animationUrl: string | undefined
-    properties: { [key: string]: string | number | boolean | Date | undefined } | undefined
+    name?: string
+    description?: string
+    imageUrl?: string
+    externalUrl?: string
+    animationUrl?: string
+    properties?: { [key: string]: string | number | boolean | Date | undefined }
 }
